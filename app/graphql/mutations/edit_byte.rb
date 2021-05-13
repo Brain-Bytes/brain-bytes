eeb90b237a0f45913f2360d5c33fc27d3d0a9942
Loïc Boset <Loic.boset@gmail.com>
2021-05-13 19:37:26 +0200

    Add GraphQL CRUD queries for Bytes

---
+++ app/graphql/mutations/edit_byte.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class EditByte < Mutations::BaseMutation
+    argument :id, ID, required: true
+    argument :params, Types::Input::ByteInputType, required: true
+
+    field :byte, Types::ByteType, null: false
+
+    def resolve(id:, params:)
+      byte_params = Hash params
+
+      begin
+        byte = Byte.find(id)
+        byte.update!(byte_params)
+
+        # The resolve method in a mutation must return a hash whose symbol matches the field names
+        { byte: byte }
+      rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+          " #{e.record.errors.full_messages.join(', ')}")
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :add_byte, mutation: Mutations::AddByte
+    field :delete_byte, mutation: Mutations::DeleteByte
+    field :edit_byte, mutation: Mutations::EditByte
   end
 end
