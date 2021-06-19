56793182abab86a98f487f63958d56caec1a2050
loicboset <loic.boset@gmail.com>
2021-06-19 16:15:26 +0200

    Fix cors attempt #1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
-    resource '*',
-      headers: %w(Authorization),
-      methods: :any,
-      expose: %w(Authorization),
-      max_age: 600
-  end
-
-  allow do
-    origins 'https://brainbytes.dev'
+    origins 'http://localhost:3001', 'https://brainbytes.dev'
     resource '*',
       headers: %w(Authorization),
       methods: :any,
