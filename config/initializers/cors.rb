1e59792fa56f569e22b77c41d354ef2c1ca32da9
loicboset <loic.boset@gmail.com>
2021-06-19 14:42:43 +0200

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins '*'
-  #   resource '*', headers: :any, methods: %i[get post patch put delete]
-  # end
   allow do
-    origins '*'
+    origins 'http://localhost:3001'
+    resource '*',
+      headers: %w(Authorization),
+      methods: :any,
+      expose: %w(Authorization),
+      max_age: 600
+  end
+
+  allow do
+    origins 'https://brainbytes.dev'
     resource '*',
       headers: %w(Authorization),
       methods: :any,
